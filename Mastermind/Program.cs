0daf3af7e3beed28c8091aceed7d24feb22a90be
Suyash <suyash.kathuria7@gmail.com>
2022-06-15 11:30:05 +1200

    Initialising Project Solution Repository

---
+++ Mastermind/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mastermind
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    
+}
