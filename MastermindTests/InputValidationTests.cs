489b07dfe3e0246366c7d593925fd88ff52ce1aa
Suyash <suyash.kathuria@myob.com>
2022-10-21 19:36:42 +1300

    Adding in Input Validation tests

---
+++ MastermindTests/InputValidationTests.cs
@@ -0,0 +1,73 @@
+using Mastermind.Presentation.InputOutput;
+using Xunit;
+
+namespace MastermindTests;
+
+public class InputValidationTests
+{
+    [Fact]
+    public void GivenValidateInputIsCalledOnAString_WhenTheStringIsSimplyQ_ThenShouldReturnTrue()
+    {
+        // Arrange
+        var validator = new ConsoleInputValidator();
+
+        // Act
+        var isValid = validator.ValidateInput("q");
+
+        // Assert
+        Assert.True(isValid);
+    }
+    
+    [Theory]
+    [InlineData("w324", false)]
+    [InlineData("12d1", false)]
+    [InlineData("qwd1", false)]
+    [InlineData("qwer", false)]
+    public void GivenValidateInputIsCalledOnAString_WhenTheStringContainsNonNumbersExceptJustQ_ThenShouldReturnFalse(string inputString, bool expectedIsValid)
+    {
+        // Arrange
+        var validator = new ConsoleInputValidator();
+    
+        // Act
+        var actualIsValid = validator.ValidateInput(inputString);
+    
+        // Assert
+        Assert.Equal(expectedIsValid, actualIsValid);
+    }
+    
+    [Theory]
+    [InlineData("1234", true)]
+    [InlineData("12", false)]
+    [InlineData("123", false)]
+    [InlineData("12345", false)]
+    [InlineData("1234567890", false)]
+    public void GivenValidateInputIsCalledOnAString_WhenTheStringContainsExactly4Characters_ThenShouldReturnTrueOtherwiseFalse(string inputString, bool expectedIsValid)
+    {
+        // Arrange
+        var validator = new ConsoleInputValidator();
+
+        // Act
+        var actualIsValid = validator.ValidateInput(inputString);
+
+        // Assert
+        Assert.Equal(expectedIsValid, actualIsValid);
+    }
+    
+    [Theory]
+    [InlineData("1239")]
+    [InlineData("6784")]
+    [InlineData("1209")]
+    [InlineData("7890")]
+    public void GivenValidateInputIsCalledOnAString_WhenTheStringContainsDigitsLessThanZeroOrMoreThanFive_ThenShouldReturnFalse(string inputString)
+    {
+        // Arrange
+        var validator = new ConsoleInputValidator();
+
+        // Act
+        var isValid = validator.ValidateInput(inputString);
+
+        // Assert
+        Assert.False(isValid);
+    }
+    
+}
