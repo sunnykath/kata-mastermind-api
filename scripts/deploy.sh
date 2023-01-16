9aaa73c553b9174752624af39b3aaa505e0f505f
Suyash <suyash.kathuria@myob.com>
2023-01-16 18:17:22 +1300

    Fixed path error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 image_tag=${BUILDKITE_COMMIT}
 
-kubectl apply -f ../kube/config.yaml -n fma
+kubectl apply -f ./kube/config.yaml -n fma
 kubectl set image -n fma deployment/suyash-mastermind-api-deployment suyash-mastermind=docker.myob.com/future-makers-academy/suyash-mastermind:$image_tag
