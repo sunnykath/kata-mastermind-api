3955cf8398b55dc0a58824fd6c480c4f8dd574cb
Suyash <suyash.kathuria@myob.com>
2023-03-09 11:49:54 +1100

    Adding in scope for the DB context

--- Mastermind.API/Program.cs
+++ Mastermind.API/Program.cs
@@ -8,16 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<MastermindContext>(opt =>
-    opt.UseInMemoryDatabase("Games"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IRandomizer, DefaultRandomizer>();
+
+builder.Services.AddScoped<DbContext, MastermindContext>();
+
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
+
+builder.Services.AddDbContext<MastermindContext>(opt =>
+    opt.UseInMemoryDatabase("Games"));
 
 var app = builder.Build();
 
